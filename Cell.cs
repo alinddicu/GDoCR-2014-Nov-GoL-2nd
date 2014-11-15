5dd840cfb8b1bf1ded1d4b5afd9b5b1cbe15f35b
alinddicu <alinddicu@yahoo.com>
2014-11-15 21:56:35 +0100

    Init

---
+++ Cell.cs
@@ -0,0 +1,43 @@
+﻿namespace Gol
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Cell
+    {
+        private bool _state;
+        private List<Cell> _neighbours;
+
+        public Cell(bool state)
+        {
+            _state = state;
+            _neighbours = new List<Cell>();
+        }
+
+        public bool IsAlive()
+        {
+            return _state;
+        }
+
+        public void AddNeighbour(Cell otherCell)
+        {
+            _neighbours.Add(otherCell);
+        }
+
+        public void ComputeState()
+        {
+            var aliveNeighbours = _neighbours.Count(n => n.IsAlive());
+            if (aliveNeighbours < 2)
+            {
+                _state = false;
+            }
+            else if (aliveNeighbours <= 3)
+            {
+                if (aliveNeighbours != 2)
+                {
+                    _state = true;
+                }
+            }
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gol
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Une_Cellule_Vivante_Avec_Moins_De_2_Voisins_Vivants_Meurt()
+        {
+            var cell = new Cell(true);
+            var otherCell1 = new Cell(true);
+            cell.AddNeighbour(otherCell1);
+            cell.ComputeState();
+            Assert.IsFalse(cell.IsAlive());
+        }
+
+        [TestMethod]
+        public void Une_Cellule_Vivante_Avec_2_Voisins_Vivants_Reste_En_Vie()
+        {
+            var cell = new Cell(true);
+            var otherCell1 = new Cell(true);
+            cell.AddNeighbour(otherCell1);
+            var otherCell2 = new Cell(true);
+            cell.AddNeighbour(otherCell2);
+            cell.ComputeState();
+            Assert.IsTrue(cell.IsAlive());
+        }
+
+        [TestMethod]
+        public void Une_Cellule_Morte_Avec_2_Voisins_Vivants_Reste_Morte()
+        {
+            var cell = new Cell(false);
+            var otherCell1 = new Cell(true);
+            cell.AddNeighbour(otherCell1);
+            var otherCell2 = new Cell(true);
+            cell.AddNeighbour(otherCell2);
+            cell.ComputeState();
+            Assert.IsFalse(cell.IsAlive());
+        }
+
+        [TestMethod]
+        public void Une_Cellule_Avec_3_Voisins_Vivants_Est_En_Vie()
+        {
+            var cell = new Cell(false);
+            var otherCell1 = new Cell(true);
+            cell.AddNeighbour(otherCell1);
+            var otherCell2 = new Cell(true);
+            cell.AddNeighbour(otherCell2);
+            var otherCell3 = new Cell(true);
+            cell.AddNeighbour(otherCell3);
+            cell.ComputeState();
+            Assert.IsTrue(cell.IsAlive());
+        }
+    }
+}
